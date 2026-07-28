19654e0617075381a5c7628637d69e7cb34b7050
Naveen kumar S <naveenkumar.s@spritle.com>
2026-07-28 11:24:24 +0530

    compose navigation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.navigation.compose)
     debugImplementation(libs.androidx.compose.ui.tooling)
 
     testImplementation(libs.junit)
